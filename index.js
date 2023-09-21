2846cca77b219c75248fefabfc2c4a1cc4b8e5cd
Khairul Ahamed Biswas <infokhairul1996@gmail.com>
2023-09-21 09:29:16 +0000

    Update 4 files add body parser

--- index.js
+++ index.js
@@ -1,41 +1,15 @@
-const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.writeHead(200, {'Content-Type':'text/html'});
-        res.write('<html>');
-        res.write('<head><title>Hello from Node</title>');
-        res.write('</head>');
-        res.write('<body><h1>Hello from Node</h1></body>');
-        res.write('</html>');
-        res.end();
-    }
-    if(url === '/user'){
-        res.writeHead(200, {'Content-Type':'text/html'});
-        res.write('<html>');
-        res.write('<head><title>Hello from Node</title>');
-        res.write('</head>');
-        res.write('<body><h1>new user</h1><form action="/createuser" method="POST"><input type="text" name="nuser"><button type="submit">submit</button></form></body>');
-        res.write('</html>');
-        res.end();
-    }
-    if(url === '/createuser'){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parseBody = Buffer.concat(body).toString();
-            const newuser = parseBody.split('=')[1];
-            fs.writeFileSync('newuser.txt', newuser);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        res.end()
-    }
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/users", (req, res, next) => {
+  console.log("Another middlewere");
+  res.send("<h1>All Users</h1>");
 });
- server.listen(8000);
+app.use("/", (req, res, next) => {
+  console.log("working middlewere");
+  res.send("<h1>Hello from Express</h1>");
+});
+app.listen(8000);
