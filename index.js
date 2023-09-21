e5f9dddbabcc3c3d3d266adf69c4c3ee244243d4
Khairul Ahamed Biswas <infokhairul1996@gmail.com>
2023-09-21 13:35:10 +0000

    Update 8 files add sjop

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+const path = require("path");
 const express = require("express");
+const rootPath = require("./util/path");
 const bodyParser = require("body-parser");
 const app = express();
 
+const adminRouter = require("./routes/admin");
+const shopRouter = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/users", (req, res, next) => {
-  console.log("Another middlewere");
-  res.send("<h1>All Users</h1>");
+app.use("/admin", adminRouter);
+app.use(shopRouter);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootPath, "views", "404.html"));
 });
-app.use("/", (req, res, next) => {
-  console.log("working middlewere");
-  res.send("<h1>Hello from Express</h1>");
-});
+
 app.listen(8000);
