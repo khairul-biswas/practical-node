e7aba8153e01d1d697896a366a0bb17fa592d627
Khairul Ahamed Biswas <infokhairul1996@gmail.com>
2023-09-22 13:18:11 +0000

    Update 8 files, add style update file

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
 const path = require("path");
 const express = require("express");
+const rootPath = require("../util/path");
 const router = express.Router();
 
+const adminData = require("../routes/admin");
+
 router.get("/", (req, res, next) => {
-  console.log("working middlewere");
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  console.log(adminData.products);
+  res.sendFile(path.join(rootPath, "views", "shop.html"));
 });
 
 module.exports = router;
